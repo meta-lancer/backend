47ec985ee90d130e87a545fd3f35196bff713eff
DanielYY95 <asrs02041@naver.com>
2023-07-13 18:20:59 +0900

    Feat Oauth Client Service 구현
    - 별도의 Controller, Service 없이 사용
    - Oauth Client 라이브러리 사용

---
+++ src/main/java/com/metalancer/backend/common/config/security/PrincipalOAuth2UserService.java
@@ -0,0 +1,80 @@
+package com.metalancer.backend.common.config.security;
+
+import com.metalancer.backend.common.constants.DataStatus;
+import com.metalancer.backend.common.constants.LoginType;
+import com.metalancer.backend.member.entity.User;
+import com.metalancer.backend.member.oauth.GoogleUserInfo;
+import com.metalancer.backend.member.oauth.KakaoUserInfo;
+import com.metalancer.backend.member.oauth.NaverUserInfo;
+import com.metalancer.backend.member.oauth.OAuth2UserInfo;
+import com.metalancer.backend.member.repository.UserRepository;
+import java.util.Map;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    //    private final BCryptPasswordEncoder encoder;
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        log.info("getAttributes : {}", oAuth2User.getAttributes());
+
+        OAuth2UserInfo oAuth2UserInfo = null;
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        LoginType loginType = LoginType.setLoginType(provider);
+
+        if (provider.equals(LoginType.KAKAO.getProvider())) {
+            oAuth2UserInfo = new KakaoUserInfo((Map) oAuth2User.getAttributes());
+        } else if (provider.equals(LoginType.GOOGLE.getProvider())) {
+            oAuth2UserInfo = new GoogleUserInfo((Map) oAuth2User.getAttributes());
+        } else if (provider.equals(LoginType.NAVER.getProvider())) {
+            oAuth2UserInfo = new NaverUserInfo((Map) oAuth2User.getAttributes().get("response"));
+        }
+
+        User user = saveOrGetUser(oAuth2UserInfo, loginType);
+
+        return new PrincipalDetails(user, oAuth2User.getAttributes());
+    }
+
+    private User saveOrGetUser(OAuth2UserInfo oAuth2UserInfo, LoginType loginType) {
+        String providerId = oAuth2UserInfo.getProviderId();
+        String email = oAuth2UserInfo.getEmail();
+        String oauthId = providerId;
+//        String nickname = oAuth2UserInfo.getName();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(loginType.toString()).append("_").append(oauthId);
+        String username = stringBuilder.toString();
+
+        Optional<User> optionalUser = userRepository.findByLoginTypeAndOauthIdAndStatus(loginType,
+            oauthId,
+            DataStatus.ACTIVE);
+        User user = null;
+
+        if (optionalUser.isEmpty()) {
+            user = User.builder()
+                .oauthId(oauthId)
+                .email(email)
+                .loginType(loginType)
+                .username(username)
+                .build();
+            userRepository.save(user);
+        } else {
+            user = optionalUser.get();
+        }
+
+        log.info("user: {}", user.toString());
+        return user;
+    }
+}

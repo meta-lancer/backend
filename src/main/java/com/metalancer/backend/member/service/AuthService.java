2b5e037781b8c8797dcf11a5f3d6aaff5ffe3b94
DanielYY95 <asrs02041@naver.com>
2023-07-14 16:03:51 +0900

    Chore 카카오 로그인 restTemplate 제거

--- src/main/java/com/metalancer/backend/member/service/AuthService.java
+++ src/main/java/com/metalancer/backend/member/service/AuthService.java
@@ -4,9 +4,5 @@ import com.metalancer.backend.member.dto.AuthRequestDTO;
 import jakarta.servlet.http.HttpSession;
 
 public interface AuthService {
-
-
-    void kakaoLogin(String code) throws Exception;
-
     boolean emailLogin(HttpSession session, AuthRequestDTO.LoginRequest dto);
 }
--- src/main/java/com/metalancer/backend/member/service/AuthServiceImpl.java
+++ src/main/java/com/metalancer/backend/member/service/AuthServiceImpl.java
@@ -5,8 +5,6 @@ import com.metalancer.backend.common.exception.BaseException;
 import com.metalancer.backend.member.domain.Email;
 import com.metalancer.backend.member.dto.AuthRequestDTO;
 import com.metalancer.backend.member.entity.User;
-import com.metalancer.backend.member.oauth.KakaoDTO;
-import com.metalancer.backend.member.oauth.KakaoOauth;
 import com.metalancer.backend.member.repository.UserRepository;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
@@ -20,23 +18,9 @@ import static com.metalancer.backend.common.constants.ObjectText.LOGIN_USER;
 @Slf4j
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
-
-    private final KakaoOauth kakaoOauth;
+ 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    @Override
-    public void kakaoLogin(String code) throws Exception {
-        String kakaoAccessToken = kakaoOauth.getKakaoInfo(code);
-        KakaoDTO kakaoTokenResponse = kakaoOauth.getUserInfoWithToken(kakaoAccessToken);
-
-//        KakaoDTO userInfo = kakaoOauth.getUserInfoWithToken(
-//            kakaoTokenResponse.get);
-//        log.info("회원 정보 입니다.{}", userInfo);
-//
-//        userService.createUser(userInfo.getKakao_account().getEmail());
-//        return BaseResponse(memberService.kakaoLogin(dto));
-    }
 
     @Override
     public boolean emailLogin(HttpSession session, AuthRequestDTO.LoginRequest dto) {

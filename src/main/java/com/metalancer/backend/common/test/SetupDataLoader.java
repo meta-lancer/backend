439209dfd8d37cdfab4b61a93e5b05b9189dc19e
DanielYY95 <asrs02041@naver.com>
2023-07-24 22:38:26 +0900

    Chore 기본 유저 mock 데이터

---
+++ src/main/java/com/metalancer/backend/common/test/SetupDataLoader.java
@@ -0,0 +1,65 @@
+package com.metalancer.backend.common.test;
+
+import com.metalancer.backend.common.constants.LoginType;
+import com.metalancer.backend.common.constants.Role;
+import com.metalancer.backend.member.entity.User;
+import com.metalancer.backend.member.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+@RequiredArgsConstructor
+public class SetupDataLoader implements ApplicationListener<ContextRefreshedEvent> {
+
+    private boolean alreadySetup = false;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private static AtomicInteger count = new AtomicInteger(0);
+
+    @Override
+    @Transactional
+    public void onApplicationEvent(final ContextRefreshedEvent event) {
+
+        if (alreadySetup) {
+            return;
+        }
+
+        setupSecurityResources();
+        User user1 = createUserIfNotFound("aaa@naver.com", "aaa111");
+        User user2 = createUserIfNotFound("aaa111@naver.com", "aaa111");
+        user1.setRole(Role.ROLE_USER);
+        user2.setRole(Role.ROLE_ADMIN);
+
+        alreadySetup = true;
+    }
+
+    private void setupSecurityResources() {
+
+
+    }
+
+    @Transactional
+    public User createUserIfNotFound(final String email,
+                                     final String password) {
+
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isEmpty()) {
+            User newUser = User.builder().email(email).password(passwordEncoder.encode(password))
+                    .loginType(LoginType.NORMAL).username(LoginType.NORMAL.getProvider() + "_" + UUID.randomUUID()
+                            .toString().substring(0, 8)).build();
+
+            return userRepository.save(newUser);
+        }
+        return user.get();
+    }
+
+}

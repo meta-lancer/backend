44588214bfa564c4de136a716183317bc6cd4bdf
DanielYY95 <asrs02041@naver.com>
2023-08-02 14:22:32 +0900

    Setting db, logging, timezone

--- src/main/java/com/metalancer/backend/BackendApplication.java
+++ src/main/java/com/metalancer/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.metalancer.backend;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class BackendApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);

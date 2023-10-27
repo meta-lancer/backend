c4a9006516f124ee854bb788ec3498fab4df74ee
DanielYY95 <asrs02041@naver.com>
2023-10-28 02:13:37 +0900

    Feat 소셜로그인 마지막 리다이렉트 설정

--- src/main/java/com/metalancer/backend/common/config/security/SecurityConfig.java
+++ src/main/java/com/metalancer/backend/common/config/security/SecurityConfig.java
@@ -1,8 +1,6 @@
 package com.metalancer.backend.common.config.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.metalancer.backend.common.constants.DataStatus;
-import com.metalancer.backend.users.entity.ApproveLink;
 import com.metalancer.backend.users.entity.User;
 import com.metalancer.backend.users.oauth.HttpCookieOAuth2AuthorizationRequestRepository;
 import com.metalancer.backend.users.repository.ApproveLinkRepository;
@@ -11,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +25,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -91,7 +89,9 @@ public class SecurityConfig {
             //OAuth 로그인
             .oauth2Login()
             .loginPage("https://www.metaovis.com") //로그
+            .defaultSuccessUrl("https://www.metaovis.com")
             .successHandler(oauth2SuccessHandler())
+            .failureHandler(authenticationFailureHandler())
             .userInfoEndpoint()
             .userService(
                 principalOAuth2UserService);//구글 로그인이 완료된(구글회원) 뒤의 후처리가 필요함 . Tip.코드x, (엑세스 토큰+사용자 프로필 정보를 받아옴)
@@ -135,19 +135,22 @@ public class SecurityConfig {
         return ((request, response, authentication) -> {
             PrincipalDetails oAuth2User = (PrincipalDetails) authentication.getPrincipal();
             User user = oAuth2User.getUser();
+
+            String targetUrl = "http://localhost:3000";
             // 이를 통해 리다이렉트하도록 응답 코드 보낸다.
-            if (DataStatus.PENDING.equals(user.getStatus())) {
-                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                response.setCharacterEncoding("UTF-8");
-                // 이미 가입링크 보낸게 있으면 분기점
-                Optional<ApproveLink> approveLink = approveLinkRepository.findByUserAndStatus(user,
-                    DataStatus.ACTIVE);
-                if (approveLink.isPresent()) {
-                    response.setStatus(HttpStatus.PROCESSING.value());
-                } else {
-                    response.setStatus(HttpStatus.PERMANENT_REDIRECT.value());
-                }
-            }
+            response.sendRedirect(targetUrl);
+        });
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return ((request, response, authenticationException) -> {
+            // Get the error message from the exception
+            String errorMessage = authenticationException.getMessage();
+
+            // Logging the error for debugging purposes
+            log.error("OAuth2 Authentication Failure: {}", errorMessage, authenticationException);
+
         });
     }
 

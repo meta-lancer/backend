48f8e907f73f1137d3338a1a495d027db27293d2
DanielYY95 <asrs02041@naver.com>
2023-11-22 15:28:12 +0900

    Modify 장바구니 assetId 추가

--- src/main/java/com/metalancer/backend/users/domain/Cart.java
+++ src/main/java/com/metalancer/backend/users/domain/Cart.java
@@ -8,13 +8,15 @@ import lombok.Getter;
 public class Cart {
 
     private final Long cartId;
+    private final Long assetId;
     private final String title;
     private final int price;
     private String thumbnail;
 
     @Builder
-    public Cart(Long cartId, String title, int price) {
+    public Cart(Long cartId, Long assetId, String title, int price) {
         this.cartId = cartId;
+        this.assetId = assetId;
         this.title = title;
         this.price = price;
     }

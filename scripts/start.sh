37df8c74f9e6b8cf546df99747047e65edf26d36
DanielYY95 <asrs02041@naver.com>
2024-02-07 20:56:54 +0900

    Setting variables as uppercase

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,17 +3,13 @@
 ROOT_PATH="/home/ubuntu/backend"
 JAR="$ROOT_PATH/meta-lancer-backend-0.0.1-SNAPSHOT.jar"
 
-APP_LOG="$ROOT_PATH/application.log"
-ERROR_LOG="$ROOT_PATH/error.log"
-START_LOG="$ROOT_PATH/start.log"
-
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
+echo "[$NOW] $JAR 복사"
 cp $ROOT_PATH/build/libs/meta-lancer-backend-0.0.1-SNAPSHOT.jar $JAR
 
-echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup nohup java -Dspring.profiles.active=dev -jar -Duser.timezone=Asis/Seoul $JAR > $APP_LOG 2> $ERROR_LOG &
+echo "[$NOW] > $JAR 실행"
+nohup nohup java -Dspring.profiles.active=dev -jar -Duser.timezone=Asis/Seoul $JAR
 
 SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+echo "[$NOW] > 서비스 PID: $SERVICE_PID"

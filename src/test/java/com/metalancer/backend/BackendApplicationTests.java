d49193fa4f46ec7d68b7d1e1ab824148cbe83709
DanielYY95 <asrs02041@naver.com>
2024-02-07 13:48:17 +0900

    Setting github action main.yml remove @SpringBootTest

--- src/test/java/com/metalancer/backend/BackendApplicationTests.java
+++ src/test/java/com/metalancer/backend/BackendApplicationTests.java
@@ -1,13 +1,12 @@
 package com.metalancer.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
